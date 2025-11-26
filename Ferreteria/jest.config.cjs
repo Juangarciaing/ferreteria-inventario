79a45afd89851ff36b405839cedd239dc0fd5547
Juan Garcia <juangarciaing@gmail.com>
2025-11-26 17:33:49 -0500

    Add API mock and environment variables for CI tests

--- Ferreteria/jest.config.cjs
+++ Ferreteria/jest.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
+    '^../lib/api$': '<rootDir>/src/__mocks__/api.ts',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.js',
